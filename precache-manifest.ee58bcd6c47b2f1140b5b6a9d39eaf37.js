9c19883d6f90217bb38c9e4876fa3bc592cbda13
Matt <matt@pop-os.localdomain>
2021-01-31 16:02:43 -0500

    Updates

---
+++ precache-manifest.ee58bcd6c47b2f1140b5b6a9d39eaf37.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4561575954a7e0fd7bb27aac1efdbc",
+    "url": "/karenkowal.github.io/index.html"
+  },
+  {
+    "revision": "e5b48cd35ee101ac7c85",
+    "url": "/karenkowal.github.io/static/css/main.d7ed120d.chunk.css"
+  },
+  {
+    "revision": "748b543d0edc62897099",
+    "url": "/karenkowal.github.io/static/js/2.c737f7f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karenkowal.github.io/static/js/2.c737f7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b48cd35ee101ac7c85",
+    "url": "/karenkowal.github.io/static/js/main.4abd3135.chunk.js"
+  },
+  {
+    "revision": "027e3036005608731c12",
+    "url": "/karenkowal.github.io/static/js/runtime-main.24b44c46.js"
+  },
+  {
+    "revision": "33045566c72b971e1940c7bf7a8671c2",
+    "url": "/karenkowal.github.io/static/media/01Director of Marketing.33045566.pdf"
+  },
+  {
+    "revision": "4da82b1cf62a18c96b99d3ebaec801b6",
+    "url": "/karenkowal.github.io/static/media/01resumepic.4da82b1c.png"
+  },
+  {
+    "revision": "73da0178423c28ab3cab65e1fb5979aa",
+    "url": "/karenkowal.github.io/static/media/02IT Product Manager Director.73da0178.pdf"
+  },
+  {
+    "revision": "4c958518166f05a18e99a2a4517fbbd2",
+    "url": "/karenkowal.github.io/static/media/02resumepic.4c958518.png"
+  },
+  {
+    "revision": "89e4a4d8013bf9f461f06c6919d2c64b",
+    "url": "/karenkowal.github.io/static/media/03CIO Resume.89e4a4d8.pdf"
+  },
+  {
+    "revision": "b1fe073c2b1ecca2dcfc40f1e67fa2c3",
+    "url": "/karenkowal.github.io/static/media/03resumepic.b1fe073c.png"
+  },
+  {
+    "revision": "01cc7eb787c8e2ce08fdf14b0c81c482",
+    "url": "/karenkowal.github.io/static/media/logo.01cc7eb7.png"
+  },
+  {
+    "revision": "80ab9be11c6e58056bcbca3172e3fe91",
+    "url": "/karenkowal.github.io/static/media/profile.80ab9be1.jpg"
+  }
+]);
